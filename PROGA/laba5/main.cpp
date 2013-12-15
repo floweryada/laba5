aa343115efd53a55efe4e83f2759824f8354b584
floweryada <floweryada@gmail.com>
2013-12-15 16:57:09 +0300

    first

---
+++ PROGA/laba5/main.cpp
@@ -0,0 +1,30 @@
+//Вариант 1.
+//Определить количество слов в тексте, если слова разделены произвольным количеством символов.
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+using namespace std;
+
+int main()
+{
+    const int L = 256;
+    char str[L];
+    cout << "\nEnter String: " << endl;
+    gets(str);
+
+    size_t strlen( const char *string );
+
+    char len;
+    len = strlen(str);
+    int N = 1;
+    for ( int i = 0; i < len; ++i)
+    {
+        if( str[i] == ' ') N += 1;
+    }
+
+    cout << "Number of words: " << N << endl;
+    return 0;
+}
