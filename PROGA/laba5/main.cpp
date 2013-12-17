d255592103a93280c296e1625adfbf7fcebaeb41
floweryada <floweryada@gmail.com>
2013-12-17 20:41:33 +0300

    try

--- PROGA/laba5/main.cpp
+++ PROGA/laba5/main.cpp
@@ -2,10 +2,10 @@
 //Определить количество слов в тексте, если слова разделены произвольным количеством символов.
 
 #include <iostream>
-#include <stdlib.h>
 #include <stdio.h>
+#include <cstring>
 
-
+int sizestr( char* str );
 using namespace std;
 
 int main()
@@ -13,18 +13,27 @@ int main()
     const int L = 256;
     char str[L];
     cout << "\nEnter String: " << endl;
-    gets(str);
+    gets( str );
 
-    size_t strlen( const char *string );
-
-    char len;
-    len = strlen(str);
+    int len;
+    len = sizestr( str );
     int N = 1;
-    for ( int i = 0; i < len; ++i)
+    for ( int i = 0; i < len; ++i )
     {
-        if(( str[i] == ' ' ) && ( str[i+1] != ' ' ) ) N += 1;
+        if(( str[i] == ' ' )&&( str[i+1] != ' ' ) ) N += 1;
     }
 
     cout << "Number of words: " << N << endl;
     return 0;
 }
+
+int sizestr( char* str )
+{
+    int count=0;
+    while( *str++ )
+    {
+        ++count;
+    }
+    return count;
+}
+
